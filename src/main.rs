00bca6e320dbc530267108bf2eee3e4ab436c140
Joren Verspeurt <joren.verspeurt@gmail.com>
2025-02-11 10:02:04 +0100

    feat: Implemented rolling buffer for availability record and improved dashboard.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::{fs, io::Write, sync::{Arc, Mutex}, time::Duration};
+use chrono::{self, Duration as ChronoDuration};
+use clap::Parser;
+use ctrlc;
+use ping_rs;
+use serde::ser::SerializeStruct;
+use serde::{Deserialize, Serialize, Serializer};
 use std::collections::HashMap;
 use std::net::IpAddr;
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
-use clap::Parser;
-use serde::{Deserialize, Serialize};
+use std::{fs, io::Write, path::PathBuf, sync::Arc, time::Duration};
+use tokio::sync::RwLock;
 use warp::Filter;
-use chrono;
-use ctrlc;
-use ping_rs;
 
 #[derive(Parser, Debug)]
 #[command(version = "1.0", about = "Monitors host availability and latency")]
@@ -33,14 +35,15 @@ struct Config {
     address: Option<String>,
     port: Option<u16>,
     log_path: Option<String>,
+    log_buffer_size: Option<usize>,
 }
 
 #[derive(Deserialize, Debug)]
 struct HostConfig {
     address: String,
-    frequency: Option<u64>, // Frequency in seconds
-    timeout: Option<u64>,   // Timeout in milliseconds
-    avail_frequency: Option<u64>, // Frequency in seconds
+    frequency: Option<u64>,         // Frequency in seconds
+    timeout: Option<u64>,           // Timeout in milliseconds
+    avail_frequency: Option<u64>,   // Frequency in seconds
     unavail_frequency: Option<u64>, // Frequency in seconds
     unavail_threshold: Option<u64>, // Threshold in number of failures
 }
@@ -56,8 +59,13 @@ impl HostConfig {
 
         let timeout = self.timeout.unwrap_or(1000);
         let min_frequency = timeout / 1000;
-        if self.get_avail_frequency() <= min_frequency || self.get_unavail_frequency() <= min_frequency {
-            return Err(format!("Host {}: All frequencies must be greater than 'timeout' / 1000", self.address));
+        if self.get_avail_frequency() <= min_frequency
+            || self.get_unavail_frequency() <= min_frequency
+        {
+            return Err(format!(
+                "Host {}: All frequencies must be greater than 'timeout' / 1000",
+                self.address
+            ));
         }
 
         Ok(())
@@ -68,11 +76,15 @@ impl HostConfig {
     }
 
     fn get_avail_frequency(&self) -> u64 {
-        self.avail_frequency.or(self.frequency).expect("Either 'avail_frequency' or 'frequency' must be specified")
+        self.avail_frequency
+            .or(self.frequency)
+            .expect("Either 'avail_frequency' or 'frequency' must be specified")
     }
 
     fn get_unavail_frequency(&self) -> u64 {
-        self.unavail_frequency.or(self.frequency).expect("Either 'unavail_frequency' or 'frequency' must be specified")
+        self.unavail_frequency
+            .or(self.frequency)
+            .expect("Either 'unavail_frequency' or 'frequency' must be specified")
     }
 
     fn get_unavail_threshold(&self) -> u64 {
@@ -80,30 +92,45 @@ impl HostConfig {
     }
 }
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone)]
 struct HostStats {
     reachable: u64,
     unreachable: u64,
     total_latency: u64,
     count: u64,
+    unavail_log: Vec<(chrono::DateTime<chrono::Local>, ChronoDuration)>,
+    log_buffer_size: usize,
 }
 
 impl HostStats {
-    fn new() -> Self {
+    fn new(log_buffer_size: usize) -> Self {
         HostStats {
             reachable: 0,
             unreachable: 0,
             total_latency: 0,
             count: 0,
+            unavail_log: Vec::with_capacity(log_buffer_size),
+            log_buffer_size,
         }
     }
 
-    fn update(&mut self, result: Option<Duration>) {
+    fn update(
+        &mut self,
+        result: Option<Duration>,
+        interruption_start: Option<chrono::DateTime<chrono::Local>>,
+    ) {
         self.count += 1;
         match result {
             Some(latency) => {
                 self.reachable += 1;
                 self.total_latency += latency.as_millis() as u64;
+                if let Some(start) = interruption_start {
+                    let duration = chrono::Local::now() - start;
+                    if self.unavail_log.len() == self.log_buffer_size {
+                        self.unavail_log.remove(0);
+                    }
+                    self.unavail_log.push((start, duration));
+                }
             }
             None => {
                 self.unreachable += 1;
@@ -117,6 +144,37 @@ impl HostStats {
         } else {
             None
         }
+    }
+
+    fn reachable_percentage(&self) -> f64 {
+        if self.count > 0 {
+            (self.reachable as f64 / self.count as f64) * 100.0
+        } else {
+            0.0
+        }
+    }
+}
+
+impl Serialize for HostStats {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("HostStats", 6)?;
+        state.serialize_field("reachable", &self.reachable)?;
+        state.serialize_field("unreachable", &self.unreachable)?;
+        state.serialize_field("total_latency", &self.total_latency)?;
+        state.serialize_field("count", &self.count)?;
+        state.serialize_field(
+            "unavail_log",
+            &self
+                .unavail_log
+                .iter()
+                .map(|(start, duration)| (start.to_rfc3339(), duration.num_seconds()))
+                .collect::<Vec<_>>(),
+        )?;
+        state.serialize_field("log_buffer_size", &self.log_buffer_size)?;
+        state.end()
     }
 }
 
@@ -147,40 +205,57 @@ async fn ping_host(address: &str, timeout: u64) -> Option<Duration> {
     let addr = address.parse().ok()?;
     let data = [1, 2, 3, 4];
     let data_arc = Arc::new(&data[..]);
-    let options = ping_rs::PingOptions { ttl: 128, dont_fragment: true };
-    let result = ping_rs::send_ping_async(&addr, Duration::from_millis(timeout), data_arc, Some(&options)).await;
+    let options = ping_rs::PingOptions {
+        ttl: 128,
+        dont_fragment: true,
+    };
+    let result = ping_rs::send_ping_async(
+        &addr,
+        Duration::from_millis(timeout),
+        data_arc,
+        Some(&options),
+    )
+    .await;
     match result {
         Ok(reply) => Some(Duration::from_millis(reply.rtt as u64)),
         Err(_) => None,
     }
 }
 
-fn run_service(config: Config, log_path: &Path, stats: Arc<Mutex<HashMap<String, HostStats>>>, stop_flag: Arc<AtomicBool>, stdout: bool, verbose: bool) {
-    let mut threads = vec![];
+async fn run_service(
+    config: Config,
+    log_path: PathBuf,
+    stats: Arc<RwLock<HashMap<String, HostStats>>>,
+    stop_flag: Arc<AtomicBool>,
+    stdout: bool,
+    verbose: bool,
+) {
+    let mut handles = vec![];
 
     for host in config.hosts {
         host.validate().expect("Invalid host configuration");
 
-        let log_path = log_path.to_path_buf();
+        let log_path = log_path.clone();
         let stats = Arc::clone(&stats);
         let stop_flag = Arc::clone(&stop_flag);
+        let log_buffer_size = config.log_buffer_size.unwrap_or(10);
 
-        threads.push(thread::spawn(move || {
-            let runtime = tokio::runtime::Builder::new_multi_thread()
-                .enable_all()
-                .build()
-                .unwrap();
+        handles.push(tokio::spawn(async move {
             let mut interruption_start: Option<chrono::DateTime<chrono::Local>> = None;
             let mut failure_count = 0;
-            let mut current_frequency;
+            let mut current_frequency = host.get_avail_frequency();
 
             while !stop_flag.load(Ordering::Relaxed) {
-                let result = runtime.block_on(ping_host(&host.address, host.get_timeout()));
-                let mut stats_guard = stats.lock().unwrap();
-                let host_stats = stats_guard.entry(host.address.clone()).or_insert_with(HostStats::new);
-                host_stats.update(result);
+                tokio::time::sleep(Duration::from_secs(current_frequency)).await;
+                let result = ping_host(&host.address, host.get_timeout()).await;
+                let mut stats_guard = stats.write().await;
+                let host_stats = stats_guard
+                    .entry(host.address.clone())
+                    .or_insert_with(|| HostStats::new(log_buffer_size));
+                host_stats.update(result, interruption_start);
 
-                let avg_latency = host_stats.average_latency()
+                let avg_latency = host_stats
+                    .average_latency()
                     .map(|lat| format!("{}ms", lat))
                     .unwrap_or_else(|| "N/A".to_string());
 
@@ -189,7 +264,9 @@ fn run_service(config: Config, log_path: &Path, stats: Arc<Mutex<HashMap<String,
                         "[{}] {} time={} mean={}",
                         chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
                         host.address,
-                        result.map(|r| format!("{}ms", r.as_millis())).unwrap_or_else(|| "unreachable".to_string()),
+                        result
+                            .map(|r| format!("{}ms", r.as_millis()))
+                            .unwrap_or_else(|| "unreachable".to_string()),
                         avg_latency
                     );
                     write_log(&log_path, &log_entry, stdout);
@@ -197,7 +274,8 @@ fn run_service(config: Config, log_path: &Path, stats: Arc<Mutex<HashMap<String,
 
                 if result.is_none() {
                     failure_count += 1;
-                    if failure_count >= host.get_unavail_threshold() && interruption_start.is_none() {
+                    if failure_count >= host.get_unavail_threshold() && interruption_start.is_none()
+                    {
                         interruption_start = Some(chrono::Local::now());
                         let log_entry = format!(
                             "[{}] {} unreachable!",
@@ -220,32 +298,26 @@ fn run_service(config: Config, log_path: &Path, stats: Arc<Mutex<HashMap<String,
                     }
                     current_frequency = host.get_avail_frequency();
                 }
-
-                if stop_flag.load(Ordering::Relaxed) {
-                    break;
-                }
-
-                thread::sleep(Duration::from_secs(current_frequency));
             }
         }));
     }
 
-    for t in threads {
-        t.join().unwrap();
+    for handle in handles {
+        handle.await.unwrap();
     }
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-
     let config = read_config(&args.config).expect("Failed to read configuration");
-    let address = config.address.clone().unwrap_or_else(|| "0.0.0.0".to_string());
+    let address = config
+        .address
+        .clone()
+        .unwrap_or_else(|| "0.0.0.0".to_string());
     let port = config.port.unwrap_or(8080);
-    let log_path_clone = config.log_path.clone();
-    let log_path = Path::new(log_path_clone.as_deref().unwrap_or("./nootnoot.log"));
-
-    let stats: Arc<Mutex<HashMap<String, HostStats>>> = Arc::new(Mutex::new(HashMap::new()));
+    let log_path = Path::new(config.log_path.as_deref().unwrap_or("./nootnoot.log")).to_path_buf();
+    let stats: Arc<RwLock<HashMap<String, HostStats>>> = Arc::new(RwLock::new(HashMap::new()));
     let stop_flag = Arc::new(AtomicBool::new(false));
 
     let stats_clone = Arc::clone(&stats);
@@ -258,23 +330,61 @@ async fn main() {
         .expect("Error setting Ctrl-C handler");
     });
 
-    let stats_filter = warp::path("dashboard").map(move || {
-        let stats_guard = stats_clone.lock().unwrap();
-        let response: Vec<_> = stats_guard.iter().map(|(host, stats)| {
-            format!(
-                "Host: {}\nReachable: {}\nUnreachable: {}\nAverage Latency: {} ms\n\n",
-                host,
-                stats.reachable,
-                stats.unreachable,
-                stats.average_latency().map_or("N/A".to_string(), |lat| lat.to_string())
-            )
-        }).collect();
-        warp::reply::html(response.join(""))
-    });
-
     tokio::spawn(async move {
-        warp::serve(stats_filter).run((address.parse::<IpAddr>().unwrap(), port)).await;
+        warp::serve(warp::path("dashboard").and_then(move || {
+            let stats_clone = Arc::clone(&stats_clone);
+            async move {
+                let stats_guard = stats_clone.read().await;
+                let response: Vec<_> =
+                    stats_guard
+                        .iter()
+                        .map(|(host, stats)| {
+                            let status_color = if stats.unreachable > 0 {
+                                "red"
+                            } else {
+                                "green"
+                            };
+                            let unavail_log: String = stats
+                                .unavail_log
+                                .iter()
+                                .map(|(start, duration)| {
+                                    format!(
+                                        "<li>{} - {} seconds</li>",
+                                        start.format("%Y-%m-%d %H:%M:%S"),
+                                        duration.num_seconds()
+                                    )
+                                })
+                                .collect();
+                            format!(
+                        "<div style='border: 1px solid black; padding: 10px; margin: 10px;'>
+                            <h2 style='color: {};'>{}</h2>
+                            <p>Reachable: {}</p>
+                            <p>Unreachable: {}</p>
+                            <p>Average Latency: {} ms</p>
+                            <p>Reachable Percentage: {:.2}%</p>
+                            <h3>Unavailability Log:</h3>
+                            <ul>{}</ul>
+                        </div>",
+                        status_color, host, stats.reachable, stats.unreachable,
+                        stats.average_latency().map_or("N/A".to_string(), |lat| lat.to_string()),
+                        stats.reachable_percentage(), unavail_log
+                    )
+                        })
+                        .collect();
+                Ok::<_, warp::Rejection>(warp::reply::html(response.join("")))
+            }
+        }))
+        .run((address.parse::<IpAddr>().unwrap(), port))
+        .await;
     });
 
-    run_service(config, log_path, Arc::clone(&stats), stop_flag, args.stdout, args.verbose);
+    run_service(
+        config,
+        log_path,
+        Arc::clone(&stats),
+        stop_flag,
+        args.stdout,
+        args.verbose,
+    )
+    .await;
 }
